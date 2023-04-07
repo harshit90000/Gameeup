f023930334a8505f896b37927839aafd084941f2
Sarthi <Sarthiraval3131@gmail.com>
2023-04-07 13:06:47 +0530

    s

--- src/screens/MenuScreen/MenuScreen.js
+++ src/screens/MenuScreen/MenuScreen.js
@@ -1,27 +1,36 @@
-import { View, Text, ScrollView } from 'react-native'
+import { View, Text, ScrollView, TouchableOpacity } from 'react-native'
 import React from 'react'
 import SimpleView from '../../components/simpleView'
 import { images } from '../../constants/imagePath'
 import { scale } from 'react-native-size-matters'
-const MenuScreen = () => {
+import navigationStrings from '../../constants/navigationStrings'
+const MenuScreen = ({ navigation }) => {
   return (
     <View>
-      <SimpleView
-        image={images.menuProfile}
-        label={"My Profile"}
-        innerStyle={{ marginTop: scale(30) }} />
+      <TouchableOpacity onPress={() => { navigation.navigate(navigationStrings.MY_PROFILE) }}>
+        <SimpleView
+          image={images.menuProfile}
+          label={"My Profile"}
+          innerStyle={{ marginTop: scale(30) }} />
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => { navigation.navigate(navigationStrings.BOOKING_SCREEN) }}>
       <SimpleView
         image={images.menuBooking}
         label={"My Bookings"}
         innerStyle={{ marginVertical: scale(20) }} />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => { navigation.navigate(navigationStrings.TERMS_SCREEN) }}>
       <SimpleView
         image={images.menuTerm}
         label={"Terms & Conditions"}
         innerStyle={{ marginBottom: scale(0) }} />
+        </TouchableOpacity>
+      <TouchableOpacity >
       <SimpleView
         image={images.menuSignOut}
         label={"Sign Out"}
         innerStyle={{ marginVertical: scale(20) }} />
+        </TouchableOpacity>
     </View>
   )
 }
